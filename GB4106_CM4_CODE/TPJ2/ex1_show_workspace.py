b4c5549bd2e317d8adfb5939ee26b050d876f6c9
Alexis GIBERT <103576080+acromtech@users.noreply.github.com>
2022-07-04 17:21:07 +0200

    Delete GB4106_CM4_CODE/TPJ2 directory

--- GB4106_CM4_CODE/TPJ2/ex1_show_workspace.py
+++
@@ -1,9 +0,0 @@
-from delta_utils import *
-
-plot_box()
-#Show working space 
-for q0 in np.linspace(-pi, pi  ,30):
-  for q1 in np.linspace(-pi, pi  ,30):
-    q=np.array([q0,q1])
-    plot_delta(np.array(q),line="o")
-plt.show()
